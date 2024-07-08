2676f50d4d7181dae040c20b0783b0a0e21c0182
David McIntosh <David.McIntosh@Softwire.com>
2024-07-08 11:46:31 +0100

    Added a scoring function for a game (a list of rounds).

--- src/scoring/score.ts
+++ src/scoring/score.ts
@@ -1,6 +1,6 @@
-import { Round } from '../models/gamestate';
+import {Round, Gamestate, BotSelection} from '../models/gamestate';
 
-export function getScoreOfRound(round:Round):number{
+export function getScoreOfRound(round:Round):-1|0|1{
     let {p1: player1Move, p2: player2Move}=round;
     const movesThatBeat={
         'R':['P', 'D'],
@@ -17,4 +17,39 @@ export function getScoreOfRound(round:Round):number{
         return -1;
     }
     return 0;
+}
+
+export function getScoreOfGame(gameState:Gamestate):number{
+    const p1Moves = gameState.rounds.map((round)=>round.p1);
+    const p2Moves = gameState.rounds.map((round)=>round.p2);
+
+    function tooMuchDynamite(playerMoves:BotSelection[]) {
+        return playerMoves.filter(x => x == 'D').length > 100;
+    }
+
+    if(tooMuchDynamite(p1Moves) && tooMuchDynamite(p2Moves)){
+        console.error("Not Implemented. Both players have used over 100 dynamite.")
+        throw new Error;
+    }
+    if(tooMuchDynamite(p1Moves)){
+        return -Infinity;
+    }
+    if(tooMuchDynamite(p2Moves)){
+        return Infinity;
+    }
+
+    let scoringRollover = 1;
+    let score = 0;
+
+    for(const round of gameState.rounds){
+        const roundScore = getScoreOfRound(round);
+        if(roundScore==0){
+            scoringRollover+=1;
+        }
+        else{
+            score+=roundScore * scoringRollover;
+            scoringRollover = 1;
+        }
+    }
+    return score;
 }
